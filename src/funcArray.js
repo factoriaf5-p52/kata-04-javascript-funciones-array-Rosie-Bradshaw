5cd832dc9058645447ec16ad14af6daecbbeb4e9
Rosie <rosie.bradshaw3@gmail.com>
2022-11-19 11:21:30 +0100

    ejercicio 3 resuelto

--- src/funcArray.js
+++ src/funcArray.js
@@ -12,12 +12,24 @@ export function maxOfTwoNumbers(num1, num2) {
     }
 
 
+
 export function findLongestWord(){}
-export function sumArray(){}
+
+
+
+export function sumArray(numbersArray){
+    const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+    let total = 0;
+    for (let number of numbersArray) {
+        total += number;
+    }
+    return total;
+}
+
 export function averageNumbers(){}
 export function averageWordLength(){}
 export function uniquifyArray(){}
-export function doesWordExist(){}
+export function doesWordExist(){} 
 export function howManyTimes(){}
 export function greatestProduct(){}
 
